a668dba567d82353a352ccceef0b3470f9df9193
Adrian Lubinski <adrian.lubinski@intel.com>
2022-05-31 13:19:21 +0200

    Port cm_bfn tests from stateful to stateless

--- src/test_suites/cm/cm_bfn/kernels/cm_bfn/bfn_genx.cpp
+++ src/test_suites/cm/cm_bfn/kernels/cm_bfn/bfn_genx.cpp
@@ -11,15 +11,15 @@ using in_t = IN_TYPE;
 
 extern "C" _GENX_MAIN_ void kernel(
 #if defined(INPUT_CONST)
-    SurfaceIndex out_buf [[type("buffer_t")]]
+    svmptr_t out_buf [[type("svmptr_t")]]
 #else
-    SurfaceIndex out_buf [[type("buffer_t")]],
+    svmptr_t out_buf [[type("svmptr_t")]],
 #if defined(INPUT_SCALAR)
     in_t in0, in_t in1, in_t in2
 #elif defined(INPUT_VECTOR)
-    SurfaceIndex in0_buf [[type("buffer_t")]],
-    SurfaceIndex in1_buf [[type("buffer_t")]],
-    SurfaceIndex in2_buf [[type("buffer_t")]]
+    svmptr_t in0_buf [[type("svmptr_t")]],
+    svmptr_t in1_buf [[type("svmptr_t")]],
+    svmptr_t in2_buf [[type("svmptr_t")]]
 #endif
 #endif
 ) {
@@ -35,29 +35,29 @@ extern "C" _GENX_MAIN_ void kernel(
   constexpr int simd = SIMD;
 #if defined(INPUT0_VEC)
   vector<in_t, simd> in0;
-  read(in0_buf, 0, in0);
+  cm_svm_block_read(in0_buf, in0);
 #elif defined(INPUT0_VEC_REF)
   vector<in_t, simd> in0_orig;
-  read(in0_buf, 0, in0_orig);
+  cm_svm_block_read(in0_buf, in0_orig);
   vector_ref<in_t, simd> in0 = in0_orig;
 #endif
 #if defined(INPUT1_VEC)
   vector<in_t, simd> in1;
-  read(in1_buf, 0, in1);
+  cm_svm_block_read(in1_buf, in1);
 #elif defined(INPUT1_VEC_REF)
   vector<in_t, simd> in1_orig;
-  read(in1_buf, 0, in1_orig);
+  cm_svm_block_read(in1_buf, in1_orig);
   vector_ref<in_t, simd> in1 = in1_orig;
 #endif
 #if defined(INPUT2_VEC)
   vector<in_t, simd> in2;
-  read(in2_buf, 0, in2);
+  cm_svm_block_read(in2_buf, in2);
 #elif defined(INPUT2_VEC_REF)
   vector<in_t, simd> in2_orig;
-  read(in2_buf, 0, in2_orig);
+  cm_svm_block_read(in2_buf, in2_orig);
   vector_ref<in_t, simd> in2 = in2_orig;
 #endif
   vector<out_t, simd> out = cm_bfn<static_cast<BFNT>(OP_NUMBER)>(in0, in1, in2);
 #endif
-  write(out_buf, 0, out);
+  cm_svm_block_write(out_buf, out);
 }
