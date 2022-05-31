5d3f3bfd73d3c133770131cb096d5e2e6fff81fe
Adrian Lubinski <adrian.lubinski@intel.com>
2022-05-31 13:36:08 +0200

    Port cm_convert tests from stateful to stateless

--- src/test_suites/cm/cm_convert/kernels/cm_convert/convert_genx.cpp
+++ src/test_suites/cm/cm_convert/kernels/cm_convert/convert_genx.cpp
@@ -12,11 +12,11 @@ using output_t = OUT_TYPE;
 
 extern "C" _GENX_MAIN_ void kernel(
 #if defined(SRC_VECTOR) || defined(SRC_MATRIX)
-    SurfaceIndex input [[type("buffer_t")]],
+    svmptr_t input [[type("svmptr_t")]],
 #elif defined(SRC_SCALAR)
     input_t in,
 #endif
-    SurfaceIndex output [[type("buffer_t")]]) {
+    svmptr_t output [[type("svmptr_t")]]) {
   constexpr int simd = SIMD;
 
 #if defined(SRC_MATRIX)
@@ -30,12 +30,12 @@ extern "C" _GENX_MAIN_ void kernel(
 #endif
 
 #if defined(SRC_VECTOR) || defined(SRC_MATRIX)
-  read(input, 0, in.template format<input_t>());
+  cm_svm_block_read(input, in.template format<input_t>());
 #endif
 
   vector<output_t, simd> out;
 
   out = CONVERT<output_t>(in);
 
-  write(output, 0, out);
+  cm_svm_block_write(output, out);
 }
