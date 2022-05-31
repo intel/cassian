447363f150e49162318c255c6da47aed545b46d1
Adrian Lubinski <adrian.lubinski@intel.com>
2022-05-31 13:41:02 +0200

    Port cm_dp tests from stateful to stateless

--- src/test_suites/cm/cm_dp/kernels/cm_dp/dp_genx.cpp
+++ src/test_suites/cm/cm_dp/kernels/cm_dp/dp_genx.cpp
@@ -15,14 +15,14 @@ using out_t = OUT_TYPE;
 using in_t = IN_TYPE;
 
 extern "C" _GENX_MAIN_ _GENX_FLOAT_CONTROL_(CM_DENORM_ALLOW) void kernel(
-    SurfaceIndex in0_buf [[type("buffer_t")]],
+    svmptr_t in0_buf [[type("svmptr_t")]],
 #if defined(INPUT1_CONST)
 #elif defined(INPUT1_SCALAR)
     in_t in1,
 #else
-    SurfaceIndex in1_buf [[type("buffer_t")]],
+    svmptr_t in1_buf [[type("svmptr_t")]],
 #endif
-    SurfaceIndex out_buf [[type("buffer_t")]]) {
+    svmptr_t out_buf [[type("svmptr_t")]]) {
 #if defined(INPUT0_MATRIX)
   constexpr int simd0_u = SIMD0_U;
   constexpr int simd0_v = SIMD0_V;
@@ -44,30 +44,30 @@ extern "C" _GENX_MAIN_ _GENX_FLOAT_CONTROL_(CM_DENORM_ALLOW) void kernel(
   vector<in_t, simd0> in0_orig;
   vector_ref<in_t, simd0> in0 = in0_orig;
 #endif
-  read(in0_buf, 0, in0.template format<in_t>());
+  cm_svm_block_read(in0_buf, in0.template format<in_t>());
 #if defined(INPUT1_MATRIX)
   constexpr int simd1_u = SIMD1_U;
   constexpr int simd1_v = SIMD1_V;
   matrix<in_t, simd1_u, simd1_v> in1;
-  read(in1_buf, 0, in1.template format<in_t>());
+  cm_svm_block_read(in1_buf, in1.template format<in_t>());
 #elif defined(INPUT1_MATRIX_REF)
   constexpr int simd1_u = SIMD1_U;
   constexpr int simd1_v = SIMD1_V;
   matrix<in_t, simd1_u, simd1_v> in1_orig;
   matrix_ref<in_t, simd1_u, simd1_v> in1 = in1_orig;
-  read(in1_buf, 0, in1.template format<in_t>());
+  cm_svm_block_read(in1_buf, in1.template format<in_t>());
 #elif defined(INPUT1_VECTOR)
   constexpr int simd1 = SIMD1;
   vector<in_t, simd1> in1;
-  read(in1_buf, 0, in1.template format<in_t>());
+  cm_svm_block_read(in1_buf, in1.template format<in_t>());
 #elif defined(INPUT1_VECTOR_REF)
   constexpr int simd1 = SIMD1;
   vector<in_t, simd1> in1_orig;
   vector_ref<in_t, simd1> in1 = in1_orig;
-  read(in1_buf, 0, in1.template format<in_t>());
+  cm_svm_block_read(in1_buf, in1.template format<in_t>());
 #elif defined(INPUT1_CONST)
   constexpr in_t in1 = INPUT1;
 #endif
   vector<out_t, simd_out> out = TEST_FUNC<out_t>(in0, in1, SAT_FLAG);
-  write(out_buf, 0, out.template format<out_t>());
+  cm_svm_block_write(out_buf, out.template format<out_t>());
 }
