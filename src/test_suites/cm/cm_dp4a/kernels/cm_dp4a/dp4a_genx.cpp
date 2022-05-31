8901f616a0882fe5fe33cb88154a9cd1090c1d4f
Adrian Lubinski <adrian.lubinski@intel.com>
2022-05-31 13:44:07 +0200

    Port cm_dp4a tests from stateful to stateless

--- src/test_suites/cm/cm_dp4a/kernels/cm_dp4a/dp4a_genx.cpp
+++ src/test_suites/cm/cm_dp4a/kernels/cm_dp4a/dp4a_genx.cpp
@@ -12,21 +12,21 @@ using acc_t = ACC_TYPE;
 using a_t = A_TYPE;
 using b_t = B_TYPE;
 
-extern "C" _GENX_MAIN_ void kernel(SurfaceIndex out_buf [[type("buffer_t")]],
-                                   SurfaceIndex acc_buf [[type("buffer_t")]],
-                                   SurfaceIndex a_buf [[type("buffer_t")]],
-                                   SurfaceIndex b_buf [[type("buffer_t")]]) {
+extern "C" _GENX_MAIN_ void kernel(svmptr_t out_buf [[type("svmptr_t")]],
+                                   svmptr_t acc_buf [[type("svmptr_t")]],
+                                   svmptr_t a_buf [[type("svmptr_t")]],
+                                   svmptr_t b_buf [[type("svmptr_t")]]) {
   constexpr int simd = SIMD;
 
   vector<acc_t, simd> acc;
   vector<a_t, simd> a;
   vector<b_t, simd> b;
 
-  read(acc_buf, 0, acc);
-  read(a_buf, 0, a);
-  read(b_buf, 0, b);
+  cm_svm_block_read(acc_buf, acc);
+  cm_svm_block_read(a_buf, a);
+  cm_svm_block_read(b_buf, b);
 
   vector<out_t, simd> out = cm_dp4a<out_t>(acc, a, b);
 
-  write(out_buf, 0, out);
+  cm_svm_block_write(out_buf, out);
 }
