76d50754fc2c552c035ce740a8f3ecd2b0aae844
Adrian Lubinski <adrian.lubinski@intel.com>
2022-06-01 13:30:07 +0200

    Port cm_svm_scattered_rw tests from stateful to stateless

--- src/test_suites/cm/cm_svm_scattered_rw/kernels/cm_svm_scattered_rw/read_svm_scattered_genx.cpp
+++ src/test_suites/cm/cm_svm_scattered_rw/kernels/cm_svm_scattered_rw/read_svm_scattered_genx.cpp
@@ -9,14 +9,14 @@
 
 using type_t = TYPE;
 
-extern "C" _GENX_MAIN_ void kernel(SurfaceIndex offset_buf [[type("buffer_t")]],
+extern "C" _GENX_MAIN_ void kernel(svmptr_t offset_buf [[type("svmptr_t")]],
                                    svmptr_t in_ptr [[type("svmptr_t")]],
-                                   SurfaceIndex out_buf [[type("buffer_t")]]) {
+                                   svmptr_t out_buf [[type("svmptr_t")]]) {
   constexpr int simd = SIMD;
   vector<type_t, simd> data;
 
   vector<unsigned, simd> offset;
-  read(offset_buf, 0, offset);
+  cm_svm_block_read(offset_buf, offset);
 
   vector<svmptr_t, simd> ptrs = in_ptr;
   ptrs = ptrs + offset;
@@ -26,7 +26,7 @@ extern "C" _GENX_MAIN_ void kernel(SurfaceIndex offset_buf [[type("buffer_t")]],
   constexpr int num_in_oword = 16 / sizeof(type_t);
   for (unsigned global_offset = 0; global_offset < simd;
        global_offset += num_in_oword) {
-    write(out_buf, global_offset * sizeof(type_t),
-          data.template select<num_in_oword, 1>(global_offset));
+    cm_svm_block_write(out_buf + global_offset * sizeof(type_t),
+                       data.template select<num_in_oword, 1>(global_offset));
   }
 }
--- src/test_suites/cm/cm_svm_scattered_rw/kernels/cm_svm_scattered_rw/write_svm_scattered_genx.cpp
+++ src/test_suites/cm/cm_svm_scattered_rw/kernels/cm_svm_scattered_rw/write_svm_scattered_genx.cpp
@@ -9,13 +9,13 @@
 
 using type_t = TYPE;
 
-extern "C" _GENX_MAIN_ void kernel(SurfaceIndex offset_buf [[type("buffer_t")]],
+extern "C" _GENX_MAIN_ void kernel(svmptr_t offset_buf [[type("svmptr_t")]],
                                    svmptr_t out_ptr [[type("svmptr_t")]]) {
   constexpr int simd = SIMD;
   vector<type_t, simd> data = {PASSED_VECTOR};
 
   vector<unsigned, simd> offset;
-  read(offset_buf, 0, offset);
+  cm_svm_block_read(offset_buf, offset);
 
   vector<svmptr_t, simd> ptrs = out_ptr;
   ptrs = ptrs + offset;
