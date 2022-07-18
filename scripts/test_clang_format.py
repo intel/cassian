aaa1e055de6bf7fca50d018ac5ba5ab5f7a56f20
Kacper Kasper <kacper.k.kasper@intel.com>
2022-07-18 09:40:13 +0200

    test_clang_format: diff for all commits from main
    * We should check format for all commits added on top of main branch,
      instead of last commit only.

--- scripts/test_clang_format.py
+++ scripts/test_clang_format.py
@@ -58,8 +58,36 @@ def find_clang_format_diff():
     raise RuntimeError("Failed to found clang-format-diff")
 
 
+def run_git_cmd(cmd, source_directory):
+    git = shutil.which("git")
+    try:
+        process = subprocess.run(
+            [git] + cmd.split(' '),
+            cwd=source_directory,
+            shell=False,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
+        logger.debug("Git command line: %s %s", git, cmd)
+    except subprocess.CalledProcessError as e:
+        logger.error("Git output:\n%s", e.stdout.decode())
+        raise
+    return process.stdout.decode().strip()
+
+
+def find_common_ancestor(source_directory):
+    last_main_commit = run_git_cmd("rev-parse remotes/origin/main", source_directory)
+    head_commit = run_git_cmd("rev-parse HEAD", source_directory)
+    if last_main_commit == head_commit:
+        return run_git_cmd("rev-parse HEAD^", source_directory)
+    return run_git_cmd(f'merge-base {last_main_commit} HEAD', source_directory)
+
+
 def test_clang_format(clang_format_diff, source_directory):
-    cmd = f'git diff -U0 --no-color HEAD^ | "{clang_format_diff}" -p1 -i && git diff --exit-code'
+    common_ancestor = find_common_ancestor(source_directory)
+    git_diff_cmd = f'git diff -U0 --no-color {common_ancestor}..HEAD'
+    cmd = f'{git_diff_cmd} | "{clang_format_diff}" -p1 -i && git diff --exit-code'
     logger.info("clang-format-diff command line: %s", cmd)
 
     process = subprocess.run(
